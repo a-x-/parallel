c7f1ad9f665797a6e89623f114e1e51804b2951a
Alexander Mekhonoshin (invntrm) <invntrm@yandex-team.ru>
2017-03-01 02:09:41 +0300

    update tests

--- index.test.js
+++ index.test.js
@@ -1,6 +1,7 @@
 Promise.all([
     require('.')({a:1}).then(a=>console.assert(a.a===1, 'then')),
     require('.')({a:1}).then(a=>console.assert(a.a!==5, 'not then')),
+    require('.')({a:1, b:2}).then(a=>console.assert(a.a===1 && a.b===2, 'then')),
     require('.')({a:Promise.reject(1)}).then(()=>{},e=>console.assert(e===1, 'catch')),
     require('.')({a:Promise.reject(1)}).then(()=>{},e=>console.assert(e!==2, 'not catch'))
 ])
